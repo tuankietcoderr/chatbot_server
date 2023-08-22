6704d6cf3a078310e85b938d10d4f861c1089e06
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2023-08-23 00:26:37 +0700

    db control

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  const isProduction = process.env.NODE_ENV === "production";
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      isProduction ? process.env.MONGODB_URI_PROD : process.env.MONGODB_URI_DEV,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("MongoDB Connected...");
   } catch (err) {
     console.error(err.message);
